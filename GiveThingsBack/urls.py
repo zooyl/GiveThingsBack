eff476100d3c584eab37ffdcd6a14fc7f11da0bf
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-03-24 09:39:52 +0100

    Front-end

--- GiveThingsBack/urls.py
+++ GiveThingsBack/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import app.views
 
 urlpatterns = [
+    path('', app.views.landing_page, name='landing-page'),
     path('admin/', admin.site.urls)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
